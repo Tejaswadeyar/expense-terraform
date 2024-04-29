3af978f27a13c2a9f5a568dcd33cb4393c1ecd53
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-30 00:55:08 +0530

    Terraform-learn = vpc creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,10 +7,22 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_subnet" "main" {
-  count      = length(var.subnets_cidr)
+resource "aws_subnet" "public" {
+  count      = length(var.public_subnets_cidr)
   vpc_id     = aws_vpc.main.id
-  cidr_block = element( var.subnets_cidr, count.index )
+  cidr_block = element( var.public_subnets_cidr, count.index )
+  availability_zone = element(var.az, count.index )
+
+  tags = {
+    Name = "subnet-${count.index}"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element( var.private_subnets_cidr, count.index )
   availability_zone = element(var.az, count.index )
 
   tags = {
@@ -51,9 +63,9 @@ data "aws_ami" "example" {
 
 
 resource "aws_instance" "test" {
-  ami           = data.aws_ami.example.image_id
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.main[0].id  # alternative = lookup(element(aws_subnet.main, 0), "id", null)
+  ami                    = data.aws_ami.example.image_id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private[0].id  # alternative = lookup(element(aws_subnet.main, 0), "id", null)
   vpc_security_group_ids = [aws_security_group.test.id]
 }
 
