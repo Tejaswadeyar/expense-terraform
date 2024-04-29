5865b3cd4b365373b4f2bec8dbf90249667ec2b7
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-29 20:47:25 +0530

    Terraform-learn = vpc creation

---
+++ modules/vpc/data.tf
@@ -0,0 +1,7 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+
+
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -17,3 +17,8 @@ resource "aws_subnet" "main" {
     Name = "subnet-${count.index}"
   }
 }
+
+resource "aws_vpc_peering_connection" "main" {
+  vpc_id        = aws_vpc.main.id
+  peer_vpc_id   = data.aws_vpc.default.id
+}
