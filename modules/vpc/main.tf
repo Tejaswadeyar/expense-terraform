020da78cefce1e90cee443a7f074e94907c82088
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-30 01:43:52 +0530

    Terraform-learn = vpc creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
 
   tags = {
