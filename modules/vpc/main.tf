4769e34ff6262070473f310cf0e244ab595270b9
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-04-30 01:20:01 +0530

    Terraform-learn = vpc creation

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,6 +6,13 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}-${var.project_name}-igw"
+  }
+}
 
 resource "aws_subnet" "public" {
   count      = length(var.public_subnets_cidr)
@@ -18,6 +25,19 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_route_table" "public" {
+  count      = length(var.public_subnets_cidr)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public-rt-${count.index+1}"
+  }
+}
 
 resource "aws_subnet" "private" {
   count      = length(var.private_subnets_cidr)
