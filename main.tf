4888034fe1acbf7c1eaf216a93c3a16907d41d87
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-05-04 18:32:35 +0530

    Terraform-expense = RDS creation

--- main.tf
+++ main.tf
@@ -35,10 +35,10 @@
 
 module "vpc" {
   source                = "./modules/vpc"
-  vpc_cidr              = var.vpc.cidr
+  vpc_cidr              = var.vpc_cidr
   public_subnets_cidr   = var.public_subnets_cidr
   web_subnets_cidr      = var.web_subnets_cidr
-  app_subnets_cidr      = var.app_subnets_cid
+  app_subnets_cidr      = var.app_subnets_cidr
   db_subnets_cidr       =  var.db_subnets_cidr
   az                    = var.az
 
