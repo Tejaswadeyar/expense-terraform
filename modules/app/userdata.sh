707639467deeccd5656dc5104d802e89a61d6a83
tejas wadeyar <Tejaswdeyar142@gmail.com>
2024-05-07 16:28:11 +0530

    Terraform-expense = user data - creation

--- modules/app/userdata.sh
+++ modules/app/userdata.sh
@@ -2,4 +2,4 @@
 
 yum install ansible python3.11-pip -y  &>>/opt/userdata.log   #for reference
 pip3.11 install botocore boto3 &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/Tejaswadeyar/expense-ansible expense.yml -e service_name=${service_name} -e env=${dev}  &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Tejaswadeyar/expense-ansible expense.yml -e service_name=${service_name} -e env=${env} &>>/opt/userdata.log
